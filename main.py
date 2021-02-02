9ef00bc3291af279ab821aac9fa222b8ab0cf727
Dmitry <52887253+Bitrys@users.noreply.github.com>
2021-02-02 21:56:27 +0500

    Add full address output line

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
+"""
+This application is a training work on WEB-7 " WEB. Знакомство с API" and pretends that this is a fucking cool map (no).
+"""
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
 from PyQt5 import uic
 
-
 import sys
 import requests
-import os
 
 
-class Map_Main(QWidget):
+class Main(QWidget):
     def __init__(self):
-        super(Map_Main, self).__init__()
+        """
+        Initialized a program.
+        """
+        super(Main, self).__init__()
         uic.loadUi('map.ui', self)
 
+        # Костыль
         self.type_of_map.setEditable(True)
         self.type_of_map.lineEdit().setAlignment(QtCore.Qt.AlignCenter)
         self.type_of_map.setEditable(False)
 
+        # setup map and connect buttons
         self.set_map()
         self.findit.clicked.connect(self.set_map)
         self.resetit.clicked.connect(self.reset_map)
 
     def set_map(self):
+        """
+        Setup map.
+        :return: no returns
+        """
         self.api_req()
 
         map_photo = QPixmap(self.map_file)
@@ -32,35 +43,48 @@ class Map_Main(QWidget):
         self.get_address()
 
     def reset_map(self):
-        file = open('config.cfg')
+        """
+        Returns the map settings to constant values.
+        :return: no returns
+        """
+        file = open('config.cfg')  # open configuration
         data = file.read()
         data = data.split('\n')
-
         self.latit_inp.setText(data[0])
         self.longit_inp.setText(data[1])
         self.spin.setValue(float(data[2]))
         self.point_to_find.setText('')
+        self.full_address.setText('')
 
         self.set_map()
 
     def api_req(self):
+        """
+        Generates an API request from the available input and generates a map to the output.
+        :return: no returns
+        """
         if self.point_to_find.text() == '':
             latitude = self.latit_inp.text()
             longitude = self.longit_inp.text()
             spn = self.spin.value()
-
         else:
             point = self.point_to_find.text()
             API_request = f'http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={point}&format=json'
             response = requests.get(API_request)
 
-            json_response = response.json()
-            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-            toponym_coodrinates = toponym["Point"]["pos"]
-            latitude, longitude = toponym_coodrinates.split(' ')
-            self.latit_inp.setText(latitude)
-            self.longit_inp.setText(longitude)
-            spn = self.spin.value()
+            if not response:
+                print('Ошибка выполнения запроса:')
+                print(API_request)
+                print('Http статус:', response.status_code, '(', response.reason, ')')
+                sys.exit(1)
+            else:
+                json_response = response.json()
+                toponym = json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+                toponym_coodrinates = toponym['Point']['pos']
+                latitude, longitude = toponym_coodrinates.split(' ')
+                self.latit_inp.setText(latitude)
+                self.longit_inp.setText(longitude)
+                spn = self.spin.value()
 
         types = {
             'Режим "Карта"': 'map',
@@ -73,9 +97,9 @@ class Map_Main(QWidget):
         response = requests.get(API_request)
 
         if not response:
-            print("Ошибка выполнения запроса:")
+            print('Ошибка выполнения запроса:')
             print(API_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print('Http статус:', response.status_code, '(', response.reason, ')')
             sys.exit(1)
         else:
             self.map_file = 'map.png'
@@ -90,11 +114,18 @@ class Map_Main(QWidget):
             point = self.point_to_find.text()
             API_request = f'http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={point}&format=json'
             response = requests.get(API_request)
-            json_response = response.json()
 
-            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-            toponym_address = toponym["metaDataProperty"]["GeocoderMetaData"]["text"]
-            self.full_address.setText(f'Полный адрес места: {toponym_address}')
+            if not response:
+                print('Ошибка выполнения запроса:')
+                print(API_request)
+                print('Http статус:', response.status_code, '(', response.reason, ')')
+                sys.exit(1)
+            else:
+                json_response = response.json()
+
+                toponym = json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+                toponym_address = toponym['metaDataProperty']['GeocoderMetaData']['text']
+                self.full_address.setText(f'Полный адрес места: {toponym_address}')
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_PageUp:
@@ -108,9 +139,10 @@ class Map_Main(QWidget):
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = Map_Main()
+    ex = Main()
     ex.show()
     sys.excepthook = except_hook
     sys.exit(app.exec())
