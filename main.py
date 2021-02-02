fb577a2d35993389b0c669da5a7ffc34bc7e2685
Dmitry <52887253+Bitrys@users.noreply.github.com>
2021-02-02 18:25:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 
 
 import sys
+import requests
+import os
 
 
 class Map_Main(QWidget):
     def __init__(self):
-        super(Main, self).__init__()
+        super(Map_Main, self).__init__()
         uic.loadUi('map.ui', self)
         self.findit.clicked.connect(self.set_map)
 
     def set_map(self):
-        request_api()
+        latitude = self.latit_inp.text()
+        longitude = self.longit_inp.text()
+        spn = self.spins.text()
 
-    def request_api(self):
-        pass
+        API_request = f'https://static-maps.yandex.ru/1.x/?ll={latitude},{longitude}&spn={spn},{spn}&l=map'
+        response = requests.get(API_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+        else:
+            map_file = 'map.png'
+
+            with open(map_file, 'wb') as file:
+                file.write(response.content)
+
+        map_photo = QPixmap(map_file)
+        map_photo = map_photo.scaled(1100, 790)
+        self.map_line.setPixmap(map_photo)
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = Main()
+    ex = Map_Main()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
